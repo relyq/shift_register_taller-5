47532ae76bb9a68992cf0f186175e1f04343b4cd
relyq <nicoale_mainenti@hotmail.es>
2021-02-13 11:51:24 -0300

    programa de prueba actualizado

--- src/main.cpp
+++ src/main.cpp
@@ -29,6 +29,7 @@ void setup() {
 
 void loop() {
   int ms_del = 100;
+
   for (uint8_t j = 0; j < 4; j++) {
     ser_set();
     for (uint8_t i = 0; i < 4; i++) {
@@ -108,6 +109,83 @@ void loop() {
   delay(ms_del);
   rtol(0, 20);
   delay(ms_del);
+
+  // no se que es esto pero esta bueno
+  for (uint8_t i = 0; i < 16; i++) {
+    ser_set();
+    srclk_pulse();
+    for (uint8_t j = 0; j < i - 1; j++) {
+      ser_clear();
+      srclk_pulse();
+    }
+    rclk_pulse();
+    delay(100);
+  }
+  // tampoco se que es esto
+  for (uint8_t i = 0; i < 16; i++) {
+    ser_clear();
+    for (uint8_t j = 15 - i; j < 16; j++) {
+      srclk_pulse();
+    }
+
+    ser_set();
+    for (uint8_t k = 0; k < 15 - i; k++) {
+      srclk_pulse();
+    }
+    rclk_pulse();
+    delay(100);
+  }
+
+  int microdel = 0;
+  int milidelay = 16;
+
+  while (1) {
+    ser_set();
+    srclk_pulse();
+    rclk_pulse();
+    if (microdel <= 16383) {
+      delayMicroseconds(microdel);
+      microdel += 1000;
+    } else {
+      delay(milidelay);
+      milidelay = (milidelay + 10) * 1.41;
+    }
+    for (uint8_t i = 0; i < 15; i++) {
+      ser_clear();
+      srclk_pulse();
+
+      rclk_pulse();
+      if (microdel <= 16383)
+        delayMicroseconds(microdel);
+      else
+        delay(milidelay);
+    }
+    for (uint8_t i = 1; i < 16; i++) {
+      ser_clear();
+      for (uint8_t j = 0; j < i + 1; j++) {
+        srclk_pulse();
+      }
+
+      if (i == 15) break;
+
+      ser_set();
+      srclk_pulse();
+      ser_clear();
+      for (uint8_t k = 0; k < 15 - i; k++) {
+        srclk_pulse();
+      }
+      rclk_pulse();
+      if (microdel <= 16383)
+        delayMicroseconds(microdel);
+      else
+        delay(milidelay);
+    }
+
+    if (milidelay >= 50) {
+      milidelay = 16;
+      microdel = 0;
+    }
+  }
 }
 
 void rtol(bool b, int del) {
@@ -132,7 +210,7 @@ void ltor(bool b, int del) {
   if (b) {
     for (uint8_t i = 0; i < 16; i++) {
       ser_set();
-      for (uint8_t j = 15 - i; j < 16; j++) {
+      for (uint8_t j = 0; j < i + 1; j++) {
         srclk_pulse();
       }
 
@@ -147,7 +225,7 @@ void ltor(bool b, int del) {
   } else {
     for (uint8_t i = 0; i < 16; i++) {
       ser_clear();
-      for (uint8_t j = 15 - i; j < 16; j++) {
+      for (uint8_t j = 0; j < i + 1; j++) {
         srclk_pulse();
       }
 
